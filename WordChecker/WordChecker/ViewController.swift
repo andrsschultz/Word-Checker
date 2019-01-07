c8ce973bfcc6492c0598296ac4b13741c13bfb95
Andreas Schultz <andreasschultz@Andreass-MacBook-Pro.local>
2019-01-07 18:15:47 +0100

    first commit

---
+++ WordChecker/WordChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordChecker
+//
+//  Created by Andreas Schultz on 07.01.19.
+//  Copyright © 2019 Andreas Schultz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
