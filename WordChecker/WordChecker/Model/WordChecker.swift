ae0865c263513c3f411eadc7fbbeacbea10f08cd
Andreas Schultz <andreasschultz@Andreass-MacBook-Pro.local>
2019-01-07 20:03:12 +0100

    point counter bug fixed

--- WordChecker/WordChecker/Model/WordChecker.swift
+++ WordChecker/WordChecker/Model/WordChecker.swift
@@ -41,35 +41,35 @@ func checkIfWordIsValid(wordToCheck: String) -> Bool {
 func pointsForWord(word: String) -> Int {
     
     let pointsForA = word.countInstances(of: "A")*1
-    let pointsForB = word.countInstances(of: "A")*3
-    let pointsForC = word.countInstances(of: "A")*4
-    let pointsForD = word.countInstances(of: "A")*1
-    let pointsForE = word.countInstances(of: "A")*1
-    let pointsForF = word.countInstances(of: "A")*4
-    let pointsForG = word.countInstances(of: "A")*2
-    let pointsForH = word.countInstances(of: "A")*2
-    let pointsForI = word.countInstances(of: "A")*1
-    let pointsForJ = word.countInstances(of: "A")*6
-    let pointsForK = word.countInstances(of: "A")*4
-    let pointsForL = word.countInstances(of: "A")*2
-    let pointsForM = word.countInstances(of: "A")*3
-    let pointsForN = word.countInstances(of: "A")*1
-    let pointsForO = word.countInstances(of: "A")*2
-    let pointsForP = word.countInstances(of: "A")*4
-    let pointsForQ = word.countInstances(of: "A")*10
-    let pointsForR = word.countInstances(of: "A")*1
-    let pointsForS = word.countInstances(of: "A")*1
-    let pointsForT = word.countInstances(of: "A")*1
-    let pointsForU = word.countInstances(of: "A")*1
-    let pointsForV = word.countInstances(of: "A")*6
-    let pointsForW = word.countInstances(of: "A")*3
-    let pointsForX = word.countInstances(of: "A")*8
-    let pointsForY = word.countInstances(of: "A")*10
-    let pointsForZ = word.countInstances(of: "A")*3
+    let pointsForB = word.countInstances(of: "B")*3
+    let pointsForC = word.countInstances(of: "C")*4
+    let pointsForD = word.countInstances(of: "D")*1
+    let pointsForE = word.countInstances(of: "E")*1
+    let pointsForF = word.countInstances(of: "F")*4
+    let pointsForG = word.countInstances(of: "G")*2
+    let pointsForH = word.countInstances(of: "H")*2
+    let pointsForI = word.countInstances(of: "I")*1
+    let pointsForJ = word.countInstances(of: "J")*6
+    let pointsForK = word.countInstances(of: "K")*4
+    let pointsForL = word.countInstances(of: "L")*2
+    let pointsForM = word.countInstances(of: "M")*3
+    let pointsForN = word.countInstances(of: "N")*1
+    let pointsForO = word.countInstances(of: "O")*2
+    let pointsForP = word.countInstances(of: "P")*4
+    let pointsForQ = word.countInstances(of: "Q")*10
+    let pointsForR = word.countInstances(of: "R")*1
+    let pointsForS = word.countInstances(of: "S")*1
+    let pointsForT = word.countInstances(of: "T")*1
+    let pointsForU = word.countInstances(of: "U")*1
+    let pointsForV = word.countInstances(of: "V")*6
+    let pointsForW = word.countInstances(of: "W")*3
+    let pointsForX = word.countInstances(of: "X")*8
+    let pointsForY = word.countInstances(of: "Y")*10
+    let pointsForZ = word.countInstances(of: "Z")*3
     
     let pointsForÄ = word.countInstances(of: "Ä")*6
-    let pointsForÜ = word.countInstances(of: "Ä")*6
-    let pointsForÖ = word.countInstances(of: "Ä")*8
+    let pointsForÜ = word.countInstances(of: "Ü")*6
+    let pointsForÖ = word.countInstances(of: "Ö")*8
 
     
 
@@ -80,10 +80,12 @@ extension String {
     func countInstances(of stringToFind: String) -> Int {
         var stringToSearch = self
         var count = 0
-        while let foundRange = stringToSearch.range(of: stringToFind, options: .diacriticInsensitive) {
+        while let foundRange = stringToSearch.range(of: stringToFind) {
             stringToSearch = stringToSearch.replacingCharacters(in: foundRange, with: "")
             count += 1
         }
+        
+        print(stringToFind, count)
         return count
     }
 }
